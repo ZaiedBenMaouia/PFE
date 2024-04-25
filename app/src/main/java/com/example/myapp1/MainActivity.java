5bce9407f9c772cb59bd9f24b124cdddc40d8796
ZaiedBenMaouia <zaiedbenmaouia@yahoo.fr>
2024-04-25 15:50:51 +0100

    ajout d'un générateur code QR

--- app/src/main/java/com/example/myapp1/MainActivity.java
+++ app/src/main/java/com/example/myapp1/MainActivity.java
@@ -6,20 +6,33 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View;
+import android.graphics.Bitmap;
+import android.widget.Toast;
 
+import com.google.zxing.WriterException;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.google.zxing.BarcodeFormat;
 
 public class MainActivity  extends AppCompatActivity {
     private Button btnLogin;
+
+    // deux bouton pour la gérération du code QR
+    private Button btnGenerate;
+    private ImageView imageQRCode;
+
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Tu peux mettre ton business ici ...
-        // Assuming your XML layout file is named activity_main.xml
         setContentView(R.layout.activity_main);
 
         // Initialize views
         btnLogin = findViewById(R.id.btn_login);
+        btnGenerate = findViewById(R.id.btnGenerate);
+        imageQRCode = findViewById(R.id.imageQRCode);
 
         // Set onClick listener for the login button
         btnLogin.setOnClickListener(v -> {
@@ -27,5 +40,24 @@ public class MainActivity  extends AppCompatActivity {
             startActivity(i);
             // Perform login logic here
         });
+
+        // Set onClick listener for the generate QR code button
+        btnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateQRCode("Mettre le texte ou le lien que tu veux générer!!!");
+            }
+        });
+    }
+
+    private void generateQRCode(String textToEncode) {
+        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+        try {
+            Bitmap bitmap = barcodeEncoder.encodeBitmap(textToEncode, BarcodeFormat.QR_CODE, 400, 400);
+            imageQRCode.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Erreur lors de la génération du QR code", Toast.LENGTH_SHORT).show();
+        }
     }
 }
